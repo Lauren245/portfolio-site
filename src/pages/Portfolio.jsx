1f7432e12ab6dc542e64d19bd7b3e01b9b1c36e2
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-01-17 16:02:28 -0800

    created placeholder card in the PortfolioCard component.

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,10 @@
+import PortfolioCard from "../components/PortfolioCard";
+
 export default function Portfolio(){
     return (
         <div>
             <h1>My Portfolio</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi commodi a ipsam consectetur provident 
-                facere quo et ullam totam porro, iste hic nesciunt accusamus ex expedita voluptate tenetur cum eaque 
-                molestiae cupiditate ipsum atque, quae deserunt amet. Aut, nisi explicabo.</p>
+            <PortfolioCard />
         </div>
     );
 }
