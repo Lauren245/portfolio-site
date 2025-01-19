06f24ad65d34a1beb812cdf7a43ec86720f233ce
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-01-18 17:17:09 -0800

    made card template.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,32 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Project(){
+// WHEN I am presented with the Portfolio section
+// THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repository
+
+    /*project card requirements:
+        1. a titled image
+        2.  GitHub link 
+        3. deployed link*/
+        
     return(
-        <div className='card' style={{width: '18rem'}}>
-            <div className='card-body'>
-                <h5 className='card-title'>Card title</h5>
-                <h6 className='card-subtitle mb-2 text-body-secondary'>Card subtitle</h6>
-                <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="card-link">Card link</a>
-            </div>
+        <div className="card text-bg-dark">
+            <img src="..." className="card-img" alt="this will be an image" />
+        <div className="card-img-overlay">
+            <span>
+                <h5 className="card-title">Card title</h5>
+                <a 
+                        href="https://github.com/Lauren245"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-white mx-2"
+                >
+                    <FontAwesomeIcon icon={faGithub} size="2x" />
+                </a>
+            </span>
+            <p className="card-text">Languages Here</p>
+        </div>
         </div>
     );
 }
