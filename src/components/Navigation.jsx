134b75d46ffd61104c6a610cd8b2f1898eb46238
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-01-22 15:31:37 -0800

    improved layout of the navbar.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,43 +1,60 @@
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom';
+import '../css/Navigation.css';
 
 function Navigation(){
     const currentPage = useLocation().pathname;
 
     return (
-        <ul className='nav nav-tabs'>
-            <li className='nav-item'>
-                <Link
-                    to='/'
-                   className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
-                > 
-                    About
-                </Link>  
-            </li>
-            <li className = 'nav-item'>
-                <Link
-                    to='/Portfolio'
-                    className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+        <nav className='navbar navbar-expand-lg bg-body-tertiary'>
+            <div className='container-fluid px-4'>
+                <a className='navbar-brand' href='/'>Lauren Moore</a>
+                <button className='navbar-toggler' type='button' data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
                 >
-                    Portfolio
-                </Link>
-            </li>
-            <li className = 'nav-item'>
-                <Link
-                    to='/Contact'
-                    className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
-                >
-                    Contact
-                </Link>
-            </li>
-            <li className = 'nav-item'>
-                <Link
-                    to='/Resume'
-                    className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
-                >
-                    Resume
-                </Link>
-            </li>
-        </ul>
+                    <span className="navbar-toggler-icon"></span>       
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
+                        <li className='nav-item mb-2'>
+                            <Link
+                                to='/'
+                                className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+                            > 
+                                About
+                            </Link>  
+                        </li>
+                        <li className = 'nav-item mb-2'>
+                            <Link
+                                to='/Portfolio'
+                                className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+                            >
+                                Portfolio
+                            </Link>
+                        </li>
+                        <li className = 'nav-item mb-2'>
+                            <Link
+                                to='/Contact'
+                                className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+                            >
+                                Contact
+                            </Link>
+                        </li>
+                        <li className = 'nav-item mb-2'>
+                            <Link
+                                to='/Resume'
+                                className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+                            >
+                                Resume
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+           
+           
+            
+
+        </nav>
     );
 }
 
