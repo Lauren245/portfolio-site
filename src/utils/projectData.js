416455f264d6e4c20ceedee1488b6988393f0790
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-04-18 16:25:20 -0700

    trying to get project images to show back up on deployed version of site.

--- src/utils/projectData.js
+++ src/utils/projectData.js
@@ -5,7 +5,7 @@
 
 const projectsArr = [
     {
-        imageSrc: "/src/assets/images/mimic-mike-demo-image.jpg",
+        imageSrc: "./src/assets/images/mimic-mike-demo-image.jpg",
         title: "Mimic Mike", 
         skills: "TypeScript, MERN Stack", 
         gitHubLink: "https://github.com/els-or/Mimic-Mike", 
@@ -13,35 +13,35 @@ const projectsArr = [
     },
     {
         
-        imageSrc: "/src/assets/images/employee-cms-demo-image.jpg",
+        imageSrc: "./src/assets/images/employee-cms-demo-image.jpg",
         title: "Employee Tracker CMS", 
         skills: "TypeScript, Inquirer.js, PostgreSQL", 
         gitHubLink: "https://github.com/Lauren245/Employee-Tracker-CMS",
         deployedLink: "#" 
     },
     {
-        imageSrc: "/src/assets/images/Weather-Dashboard-Demo-Image.jpg",
+        imageSrc: "./src/assets/images/Weather-Dashboard-Demo-Image.jpg",
         title: "Weather Dashboard",
         skills: "TypeScript",
         gitHubLink: "https://github.com/Lauren245/Weather-Dashboard",
         deployedLink: "https://weather-dashboard-ktb0.onrender.com/"
     },
     {
-        imageSrc: "/src/assets/images/wevents-demo-image.jpg",
+        imageSrc: "./src/assets/images/wevents-demo-image.jpg",
         title: "Wevents",
         skills: "TypeScript, 3rd-Party APIs, routes",
         gitHubLink: "https://github.com/Lauren245/Wevents",
         deployedLink: "https://wevents.onrender.com/" //!!! App is deployed, but currently does not work. The Database needs to be refactored
     },
     {
-        imageSrc: "/src/assets/projectImages/book-search-engine-demo-image.jpg",
+        imageSrc: "./src/assets/projectImages/book-search-engine-demo-image.jpg",
         title: "Book Search Engine",
         skills: "TypeScipt, React, 3rd-Party APIs, GraphQL, MongoDB",
         gitHubLink: "https://github.com/Lauren245/Book-Search-Engine",
         deployedLink: "https://book-search-engine-62x0.onrender.com"
     },
     {
-        imageSrc: "/src/assets/projectImages/candidate-search-demo-image.jpg",
+        imageSrc: "./src/assets/projectImages/candidate-search-demo-image.jpg",
         title: "Candidate Search",
         skills: "TypeScript, React, Vite, CSS, 3rd-Party API",
         gitHubLink: "https://github.com/Lauren245/Candidate-Search",
